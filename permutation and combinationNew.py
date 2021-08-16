3886ac378f8fd00fdbe750322d7d53c2e5c0ebf5
Ziyao22 <70288628+Ziyao22@users.noreply.github.com>
2021-08-16 01:34:37 -0500

    Update permutation and combinationNew.py

--- permutation and combinationNew.py
+++ permutation and combinationNew.py
@@ -1,27 +1,24 @@
-total=0
+orders=0
 books=[0,0,0,1,2,3,4,5,6,7]
-count=len(books)
+a=len(books)
 for i in range(0,8):
- for j in range(i+1,8):
- for k in range(j+1,8):
- List=[1,1,1,1,1,1,1,1]
- List[i]=0
- List[j]=0
- List[k]=0
- for l in range(count):
- if l!=k and l!=i and l!=j:
- for a in range(count):
- if a!=k and a!=i and a!=j and a!=l:
- for b in range(count):
- if b!=k and b!=i and b!=j and b!=l and b!=a:
- for c in range(count):
- if c!=k and c!=i and c!=j and c!=l and c!=a
-and c!=b:
- for d in range(count):
- if d!=k and d!=i and d!=j and d!=l and
-d!=a and d!=b and d!=c:
- total+=1
- if total<=100:
- print
-(books[i],books[j],books[k],books[l],books[a],books[b],books[c],books[d])
-print("The possibilities of outcomes are : ", total)
+    for j in range(i+1,8):
+        for k in range(j+1,8):
+            List=[1,1,1,1,1,1,1,1]
+            List[i]=0
+            List[j]=0
+            List[k]=0
+            for l in range(a):
+                if l!=k and l!=i and l!=j:
+                    for m in range(a):
+                        if m!=k and m!=i and m!=j and m!=l:
+                            for n in range(a):                                        
+                                if n!=k and n!=i and n!=j and n!=l and n!=m:
+                                    for o in range(a):
+                                        if o!=k and o!=i and o!=j and o!=l and o!=m and o!=n:
+                                            for p in range(a):
+                                                if p!=k and p!=i and p!=j and p!=l and p!=m and p!=n and p!=o:
+                                                    orders+=1
+                                                    if orders<101:
+                                                        print (books[i],books[j],books[k],books[l],books[m],books[n],books[o],books[p])
+print("The total count is : ", orders)
