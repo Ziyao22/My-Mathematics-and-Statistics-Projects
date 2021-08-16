13869266d7ae89f02dba676eac07c120dd5f1c7b
Ziyao22 <70288628+Ziyao22@users.noreply.github.com>
2021-08-16 01:34:18 -0500

    Update permutation and combination.py

--- permutation and combination.py
+++ permutation and combination.py
@@ -1,31 +1,31 @@
 int orders = 0;
-String[] books = {"calculus","physics", "chemistry", "history", "literature",
-"french", "astronomy"};
-for(String a : books)
-{
-for(String b : books)
-{
-if(a.compareTo(b) < 0)
-{
-for(String c : books)
-{
-if(a.compareTo(c) < 0 && b.compareTo(c) < 0)
-{
-for(String d : books)
-{
-if(a.compareTo(d) < 0 &&
-b.compareTo(d) < 0 &&
-c.compareTo(d) < 0)
-{
-System.out.println(a + " " +
-b + " " + c + " " +
-d);
-orders++;
-}
-}
-}
-}
-}
-}
-}
-System.out.println("Final count of all the possibilities: " + orders);
+		String[] books = {"calculus","physics", "chemistry", "history", "literature", 
+    "french", "astronomy"};
+		for(String a : books)
+		{
+			for(String b : books)
+			{
+				if(a.compareTo(b) < 0)
+				{
+					for(String c : books)
+					{
+						if(a.compareTo(c) < 0 && b.compareTo(c) < 0)
+						{
+							for(String d : books)
+							{
+								if(a.compareTo(d) < 0 && 
+   b.compareTo(d) < 0 && 
+   c.compareTo(d) < 0)
+								{
+									System.out.println(a + " " + 
+b + " " + c + " " + d);
+									orders++;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		System.out.println("Final count of all the possibilities: " + orders);
+
